474ecf79e6c72fc8ba2e8d52e3e1bb6a717ee614
DerianAguilar <116276342+DerianAguilar@users.noreply.github.com>
2023-06-25 15:13:11 -0500

    cambios

--- BackendOtdr/src/main/java/com/otdr/otdr/Repositories/FalloRepository.java
+++ BackendOtdr/src/main/java/com/otdr/otdr/Repositories/FalloRepository.java
@@ -2,8 +2,14 @@ package com.otdr.otdr.Repositories;
 
 import com.otdr.otdr.Data.Entidades.Fallo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FalloRepository extends JpaRepository<Fallo, Long> {
 
+
+    @Query(
+            nativeQuery = true,
+            value = "SELECT * FROM fallo f WHERE f.fallo_nombre= :nombre"
+    )
     public Fallo findByFalloNombre(String nombre);
 }
